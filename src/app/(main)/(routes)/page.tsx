bdaf1d74811fe90a39ea9ee2c890d16c34e034df
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-28 12:46:34 +0530

    feat: clerk sign in & up setup

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  const state = false;
+  return (
+    <main className="flex items-center justify-center">
+      <h1 className={cn("text-6xl", state && "text-sm")}>Dalpat Rathore</h1>
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </main>
+  );
+}
