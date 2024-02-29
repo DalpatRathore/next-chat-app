7b8fecf35ca01c11406b27684933be8412345f3d
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-29 15:05:15 +0530

    feat: leave server functionality

--- src/components/server/ServerHeader.tsx
+++ src/components/server/ServerHeader.tsx
@@ -83,7 +83,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className=" text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("leaveServer", { server })}
+          >
             Leave Server
             <LogOut className="w-4 h-4 ml-auto"></LogOut>
           </DropdownMenuItem>
--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -7,6 +7,7 @@ import InviteModal from "@/components/modals/InviteModal";
 import EditServerModal from "@/components/modals/EditServerModal";
 import MembersModal from "@/components/modals/MembersModal";
 import CreateChannelModal from "@/components/modals/CreateChannelModal";
+import LeaveServerModal from "@/components/modals/LeaveServerModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -26,6 +27,7 @@ const ModalProvider = () => {
       <EditServerModal></EditServerModal>
       <MembersModal></MembersModal>
       <CreateChannelModal></CreateChannelModal>
+      <LeaveServerModal></LeaveServerModal>
     </>
   );
 };
